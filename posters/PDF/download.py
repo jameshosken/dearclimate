43390f2fd8c5eb962e5e2c6bd21e73be4dd8d7e1
James Hosken <jameswfhosken@gmail.com>
2018-02-26 14:42:42 -0600

    Add posters

---
+++ posters/PDF/download.py
@@ -0,0 +1,21 @@
+import requests
+
+
+
+
+
+for i in range(1, 73):
+	url = "http://www.dearclimate.net/img/pdfs/" + str(i) + ".pdf"
+	f = "poster-" + str(i) + ".pdf"
+
+	r = requests.get(url, allow_redirects=True)
+	open(f, 'wb').write(r.content)
+	print(f, "done")
+
+	# urllib.request.urlretrieve(url, f)
+
+	# testfile = urllib.URLopener()
+	# testfile.retrieve(url, f)
+	
+	# file_url = "http://www.dearclimate.net/img/pdfs/" + str(i) + ".pdf"
+	# file_name = wget.download(file_url)
